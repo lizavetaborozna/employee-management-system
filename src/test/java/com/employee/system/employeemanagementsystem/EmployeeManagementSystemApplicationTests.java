7f6c45f18a1cbdde2e95818a6d9cf08ed97855bd
Lizaveta Barazna <lizavetaborozna@gmail.com>
2023-04-01 16:35:09 +0300

    Initial commit

---
+++ src/test/java/com/employee/system/employeemanagementsystem/EmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee.system.employeemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
